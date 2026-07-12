93f67d4bcbbcda5c5549490a4c2ae2f0d388910c
yousfkhaled202020-cpu <yousfkhaled202020@gmail.com>
2026-07-13 02:32:50 +0300

    feat: Add routes to the server

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
+//requiring packages and files to combine then in server 
 const dotenv = require("dotenv").config();
 const connectDb = require("./db/connect.js");
 const express = require("express");
 const mongoSanitize = require("express-mongo-sanitize");
 const errorHandler = require("./middleware/errorHandler.js");
-
-
+const categoriesRouter = require("./routes/category.routes.js");
+const productsRouter = require("./routes/product.routes.js");
+const cartRouter = require("./routes/cart.routes.js");
+const orderRouter = require("./routes/order.routes.js");
 
 
 const app = express();
 app.use(express.json())
 app.use(mongoSanitize())
 
+// running routes
+app.use("/api/categories", categoriesRouter);
+app.use("/api/products", productsRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
-async function runServer() {
-    await connectDb();
-    const port = process.env.PORT;
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    })
-}
+// 404 error handler 
 app.use((req, res, next) => {
     const error = new Error(`Not Found -  ${req.originalUrl}`);
     error.statusCode = 404;
     next(error);
 })
-app.use(errorHandler);
 
+//error handler
+app.use(errorHandler);
+//function to run server 
+async function runServer() {
+    await connectDb(); // run database first as it takes much time 
+    const port = process.env.PORT;
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    })
+}
 
 runServer();
