e5d19e409ec43cc1cc105bbce5a25c7fac65bc67
yousfkhaled202020-cpu <yousfkhaled202020@gmail.com>
2026-07-13 02:13:52 +0300

    feat: create app.js project server

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-require("dotenv").config();
-const connectDb = require("./database/connect.js");
+const dotenv = require("dotenv").config();
+const connectDb = require("./db/connect.js");
 const express = require("express");
-async function runServer(){
- await connectDb();
+const mongoSanitize = require("express-mongo-sanitize");
+
+
+
+
 const app = express();
-const port = process.env.PORT;
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+app.use(express.json())
+app.use(mongoSanitize())
+
+
+async function runServer() {
+    await connectDb();
+    const port = process.env.PORT;
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    })
 }
+
+
 runServer();
+git
