8dc9a91c0467a98933e156028312e0f48f4f0455
yousfkhaled202020-cpu <yousfkhaled202020@gmail.com>
2026-07-13 01:37:45 +0300

    feat: category schema with name , description , slug and timestamp

---
+++ models/category.model.js
@@ -0,0 +1,28 @@
+// requiring the packages
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+//Category Schema
+
+const categorySchema = new Schema({
+    name: {
+        type: String,
+        required:[true ,"Please Enter the Name of the Category"],
+        unique: true,
+        trim: true
+    },
+    description: {
+        type: String
+    },
+    slug: {
+        type: String,
+        required:[true ,"Please Enter the slug of the Category"],
+        unique: true,
+        trim: true
+    }
+},
+    {
+        timestamps: true
+    });
+
+//exporting schema
+module.exports = mongoose.model('Category', categorySchema);
