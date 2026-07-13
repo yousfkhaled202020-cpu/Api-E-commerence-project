220f494b5037d4083f0ca3a7382778cbcc2df35d
yousfkhaled202020-cpu <yousfkhaled202020@gmail.com>
2026-07-13 03:03:22 +0300

    feat: Create cart and orders routes

---
+++ routes/cart.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const cartRouter = express.Router();
+const cartController = require("../controllers/cart.controller.js");
+cartRouter.get("/" , cartController.getCart);
+cartRouter.delete("/items/:id", cartController.deleteItemById);
+cartRouter.post("/items", cartController.createNewItem);
+cartRouter.patch("/items/:id", cartController.updateItem);
+cartRouter.delete("/", cartController.deleteCart);
+module.exports = cartRouter ;
