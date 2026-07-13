0d7b75bc067b2f386e1f69fba9b37c4db1811ee3
yousfkhaled202020-cpu <yousfkhaled202020@gmail.com>
2026-07-13 03:16:14 +0300

    docs: formatting routes files

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const orderRouter = express.Router();
 const orderController = require("../controllers/order.controller.js");
-orderRouter.get("/" , orderController.getAllOrders);
+orderRouter.get("/", orderController.getAllOrders);
 orderRouter.get("/:id", orderController.getOrderById);
 orderRouter.post("/", orderController.createNewOrder);
 orderRouter.patch("/:id/status", orderController.updateOrder);
-module.exports = orderRouter ;
+module.exports = orderRouter;
